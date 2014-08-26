2cabf7c0df6a9e41ca21af8a550e49e756037576
liobouchan <leo.bouchan@gmail.com>
2014-08-26 16:18:55 -0500

    codigo arreglado

--- Practica2/cheque.java
+++ Practica2/cheque.java
@@ -62,156 +62,156 @@ public class cheque{
             palabra+="dieciseis ";
           if(num.charAt(auxiliar+1)=='7')
             palabra+="diecisiete ";
-          if(num charAt(auxiliar+1)=='8')
+          if(num.charAt(auxiliar+1)=='8')
             palabra+="dieciocho ";
           if(num.charAt(auxiliar+1)=='9')
             palabra+="diecinueve ";
           decena++;
           identificador--;
         } 
-      }
-          if(numero==2)
-          palabra+="veinti";
+        if(numero==2){
+            palabra+="veinti";
           if(numero==3)
-          palabra+="treinta y ";
+            palabra+="treinta y ";
           if(numero==4)
-          palabra+="cuarenta y ";
-          palabra-="cincuenta y ";
-          if(numero==6) if(numero==7)
-          palabra+="sesenta y ";
+            palabra+="cuarenta y ";
+          if(numero==5)
+            palabra+="cincuenta y ";
+          if(numero==6) 
+            palabra+="sesenta y ";
+          if(numero==7)
+            palabra+="setenta y ";
           if(numero==8)
-          palabra+="ochenta y ";
+            palabra+="ochenta y ";
           if(numero==9)
-          palabra+="noventa y ";
+            palabra+="noventa y ";
           break;
           }
-    case 3:
-    {
+      }
+    case 3: {
     if(numero==1)
-    palabra+="ciento ";
+        palabra+="ciento ";
     if(numero==2)
-    palabra+="doscientos ";
-    if(numero==3) if(numero==4)
-    palabra+="cuatrocientos ";
-    if(numero==5)
-    palabra+="quinientos ";
-    if(numero==6)
-    palabra+="seiscientos ";
-    if(número==7)
-    palabra+="setecientos ";
-    if(numero==8)
-    palabra+="ochocientos ";
-    if(numero==9)
-    palabra+="novecientos ";
-    break;
-    }
-    case 3:
-    {
-    if(mil!=1)
-    {
-    if(numero==1)
-    palabra+="un mil ";
-    if(numero==2)
-    palabra+="dos mil ";
+        palabra+="doscientos ";
     if(numero==3)
-    palabra+="tres mil ";
+       palabra+="trescientos ";
     if(numero==4)
-    palabra+="cuatromil "
+        palabra+="cuatrocientos ";
     if(numero==5)
-    palabra+="cincomil ";
+        palabra+="quinientos ";
     if(numero==6)
-    palabra+="seismil ";
+        palabra+="seiscientos ";
     if(numero==7)
-    palabra+="sietemil ";
+        palabra+="setecientos ";
     if(numero==8)
-    palabra+="ochomil ";
+        palabra+="ochocientos ";
     if(numero==9)
-    palabra+="nuevemil ";
-    }
+        palabra+="novecientos ";
     break;
     }
-    case 5:
-    {
-    if(numero==1)
-    {
-    if(num.charAt(auxiliar+1)=='1')
-    palabra+="once mil ";
-    if(num.charAt(auxiliar+1)=='2')
-    palabra+="doce mil";
-    if(num.charAt(auxiliar+1)=='3')
-    palabra+="trece mil";
-    if(num.charAt(auxiliar+2)=='4')
-    palabra+="catorce mil";
-    if(num.charAt(auxiliar+1)=='5')
-    palabra+="quince mil";
-    if(num.charAt(auxiliar+1)=='6')
-    palabra+="dieciseis mil";
-    if(num.charAt(auxiliar+1)=='7')
-    palabra+="diecisiete mil
-    if(num.charAt(auxiliar+1)=='8')
-    palabra+="dieciocho mil";
-    if(num.charAt(auxiliar+1)=='9')
-    palabra+="diecinueve mil";
-    mil++;
-    identificador--;
+    case 4:{
+        if(mil!=1){
+            if(numero==1)
+                palabra+="un mil ";
+            if(numero==2)
+                palabra+="dos mil ";
+            if(numero==3)
+                palabra+="tres mil ";
+            if(numero==4)
+                palabra+="cuatromil ";
+            if(numero==5)
+                palabra+="cincomil ";
+            if(numero==6)
+                palabra+="seismil ";
+            if(numero==7)
+                palabra+="sietemil ";
+            if(numero==8)
+                palabra+="ochomil ";
+            if(numero==9)
+                palabra+="nuevemil ";
+        }
+        break;
+        }
+    case 5:{
+    if(numero==1){
+        if(num.charAt(auxiliar+1)=='1')
+            palabra+="once mil ";
+        if(num.charAt(auxiliar+1)=='2')
+            palabra+="doce mil";
+        if(num.charAt(auxiliar+1)=='3')
+            palabra+="trece mil";
+        if(num.charAt(auxiliar+2)=='4')
+            palabra+="catorce mil";
+        if(num.charAt(auxiliar+1)=='5')
+            palabra+="quince mil";
+        if(num.charAt(auxiliar+1)=='6')
+            palabra+="dieciseis mil";
+        if(num.charAt(auxiliar+1)=='7')
+            palabra+="diecisiete mil";
+        if(num.charAt(auxiliar+1)=='8')
+            palabra+="dieciocho mil";
+        if(num.charAt(auxiliar+1)=='9')
+        palabra+="diecinueve mil";
+        mil++;
+        identificador--;
     }
+    if(numero==2){
+        palabra+="veinte y ";
+        if(numero==3)
+            palabra+="treinta y ";
+        if(numero==4)
+            palabra+="cuarenta y ";
+        if(numero==5)
+            palabra+="cincuenta y ";
+        if(numero==6) if(numero==7)
+            palabra+="setenta y ";
+        if(numero==8)
+            palabra+="ochenta y ";
+        if(numero==9)
+            palabra+="noventa ";
+    }
+        break;
+    }
+    case 6:{
+    if(numero==1)
+        palabra+="ciento ";
     if(numero==2)
-    palabra+="veinte y ";
+        palabra+="doscientos ";
     if(numero==3)
-    palabra+="treinta y ";
+        palabra+="trescientos ";
     if(numero==4)
-    palabra+="cuarenta y ";
+        palabra+="cuatrocientos ";
     if(numero==5)
-    palabra+="cincuenta y ";
-    if(numero==6) if(numero==7)
-    palabra+="setenta y ";
-    if(numero==8)
-    palabra+="ochenta y ";
-    if(numero==9)
-    palabra+="noventa ";
-    break;
-    }
-    } case 6:
-    {
-    if(numero==1)
-    palabra+="ciento ";
-    if(numero==2)
-    palabra+="doscientos ";
-    if(numero==3)
-    palabra+="trescientos ";
-    if(numero==4)
-    palabra+="cuatrocientos ";
-    if(numero==5))
-    palabra+="quinientos ";
+        palabra+="quinientos ";
     if(numero==6)
-    palabra+="seiscientos ";
+        palabra+="seiscientos ";
     if(numero==7)
-    palabra+="setecientos ";
+        palabra+="setecientos ";
     if(numero==8)
-    palabra+="ochocientos ";
+        palabra+="ochocientos ";
     if(numero==9)
-    palabra+="novecientos ";
+        palabra+="novecientos ";
     break;
     }
-    case 7:
-    {
+    case 7:{
     if(numero==1)
-    palabra+="un millon ";
+        palabra+="un millon ";
     if(numero==2)
-    palabra+="dos millones ";
+        palabra+="dos millones ";
     if(numero==3)
-    palabra+="tres millones ";
+        palabra+="tres millones ";
     if(numero==4)
-    palabra+="cuatro millones ";
+        palabra+="cuatro millones ";
     if(numero==5)
-    palabra+="cinco millones ";
+        palabra+="cinco millones ";
     if(numero==6)
-    palabra+="seis millones ";
+        palabra+="seis millones ";
     if(numero==7)
-    palabra+="siete millones ";
+        palabra+="siete millones ";
     if(numero==8)
-    palabra+="ocho millones ";
-    if(numero==9) break;
+        palabra+="ocho millones ";
+    if(numero==9) 
+        break;
     }
     }
     }
@@ -219,28 +219,30 @@ public class cheque{
     {
     int longitud=num.length();
     int numero;
-    int identificador=longitud
+    int identificador=longitud;
 
-    while(auxiliar {
+    while(auxiliar!=0) {
     char caracter=num.charAt(auxiliar);
     numero=Integer.parseInt(caracter+"");
-    identifica(numero,identificdor);
+    identifica(numero,identificador);
     identificador--;
     auxiliar++;
     }
     }
-  //programa principal public static void main (String args[]) { String nombre="";
+  //programa principal 
+    public static void main (String args[]) { 
 
+  String nombre="";
   System.out.println("Dame tu nombre: "); nombre=leer(); System.out.println("Dame la cantidad: "); num=leer(); 
   leenumero(); System.out.println();
   System.out.println();
   System.out.println();
   System.out.println();
   System.out.println();
-  System.out.printl();
+  System.out.println();
   System.out.println("Banco Patito de Mexico \t\t\t\t\t\tFecha:_________________");
   System.out.println("Nombre: "+nombre);
   System.out.println("Cantidad c/letra: "+palabra+"\t\t"+"$ "+num);
   System.out.println();
   System.out.println("Firma:_________________");
-
+   }
