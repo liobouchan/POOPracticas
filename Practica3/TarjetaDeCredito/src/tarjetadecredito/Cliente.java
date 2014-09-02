c8c1834b9af39cdbd9f1ba6dc540fe4d3a6db79a
liobouchan <leo.bouchan@gmail.com>
2014-09-02 01:08:48 -0500

    Starts with CreditCard

---
+++ Practica3/TarjetaDeCredito/src/tarjetadecredito/Cliente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tarjetadecredito;
+
+/**
+ *
+ * @author liodebian
+ */
+public class Cliente {
+    private int id;
+    private int saldo;
+    private int[] articulos = new int[10000];
+    private int total;
+    
+}
